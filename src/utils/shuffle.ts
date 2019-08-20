af57345310161d085b67638ec5cbcadc30d90d04
Васильев Дмитрий <dvasiliev97@yandex.ru>
2019-08-20 17:39:41 +0300

    fix(shuffle): make shuffle pure

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -7,17 +7,18 @@
  */
 // eslint-disable-next-line
 const shuffle = <T>(array: T[]): T[] => {
-  let m: number = array.length;
+  const arrayShuffled = array.slice(0);
+  let m: number = arrayShuffled.length;
   let i: number;
 
   while (m) {
     i = Math.floor(Math.random() * m);
     m -= 1;
     // eslint-disable-next-line no-param-reassign
-    [array[m], array[i]] = [array[i], array[m]];
+    [arrayShuffled[m], arrayShuffled[i]] = [arrayShuffled[i], arrayShuffled[m]];
   }
 
-  return array;
+  return arrayShuffled;
 };
 
 export default shuffle;
