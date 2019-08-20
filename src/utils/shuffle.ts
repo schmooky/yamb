d2008bf8b9e0d177f43fbe2d4ea59c94756d6fb2
Ruslan Povolockii <devianllert@gmail.com>
2019-08-20 19:26:29 +0300

    fix(shuffle): fix eslint errors

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -3,22 +3,23 @@
  *
  * @see https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
  * @param  {T[]} array Массив который надо перетасовать
- * @returns T Перетасованный массив
+ * @returns {T[]} Перетасованный массив
  */
-// eslint-disable-next-line
-const shuffle = <T>(array: T[]): T[] => {
-  const arrayShuffled = array.slice(0);
+
+function shuffle <T>(array: T[]): T[] {
+  const arrayShuffled = [...array];
+
   let m: number = arrayShuffled.length;
   let i: number;
 
   while (m) {
     i = Math.floor(Math.random() * m);
     m -= 1;
-    // eslint-disable-next-line no-param-reassign
+
     [arrayShuffled[m], arrayShuffled[i]] = [arrayShuffled[i], arrayShuffled[m]];
   }
 
   return arrayShuffled;
-};
+}
 
 export default shuffle;
